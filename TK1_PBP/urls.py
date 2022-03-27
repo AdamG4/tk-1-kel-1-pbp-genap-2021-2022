f03f6b9be5932d390cd581fe89318874fbccc131
AdamGhaviyasha <adam.ghaviyasha@ui.ac.id>
2022-03-27 10:47:20 +0700

    Django installation + homepage app

---
+++ TK1_PBP/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.contrib import admin
+
+import homepage.urls as homepage
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(homepage)),
+]
