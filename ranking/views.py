d670e3dbf07928f6f8c725367e5a4a0415319242
satrna1 <satrna0@gmail.com>
2022-04-22 03:23:59 +0700

    New change in ranking app

--- ranking/views.py
+++ ranking/views.py
@@ -1,3 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+from .models import FootballPlayer
 # Create your views here.
+def index(request):
+    player = FootballPlayer.objects.all().values()
+    template = loader.get_template('rankHome.html')
+    context = {
+        'player': player
+    }
+    return HttpResponse(template.render(context, request))
